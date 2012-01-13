8992efe289a81c34f18c95b495cda69232318fa8
Davi Alexandre <davi@davialexandre.com.br>
2012-01-13 00:51:52 -0200

    Some indentation fixes and optimizations removing unnecessary code

--- AttributesBackupBehavior.php
+++ AttributesBackupBehavior.php
@@ -2,48 +2,52 @@
 
 class AttributesBackupBehavior extends CActiveRecordBehavior {
 
-    private $oldAttributes;
-    
+    private $oldAttributes = array();
+
     public $reloadAfterSave = true;
-    
+
     public function afterFind($event) {
         parent::afterFind($event);
-    	$this->getAttributesFromOwner();
+        $this->getAttributesFromOwner();
     }
-    
+
     public function afterSave($event) {
-    	parent::afterSave($event);
-    	if($this->reloadAfterSave) {
+        parent::afterSave($event);
+        if($this->reloadAfterSave) {
             $this->getAttributesFromOwner();
         }
     }
-    
+
     private function getAttributesFromOwner() {
         $this->oldAttributes = array();
-	    foreach($this->owner->attributes as $name => $value) {
+        foreach($this->owner->attributes as $name => $value) {
             $this->oldAttributes[$name] = $value;
         }
     }
-    
+
     public function attributesChanged() {
+        if(empty($this->oldAttributes)) {
+            return false;
+        }
+        
         $number_of_changed_attributes = count(array_diff($this->owner->attributes, $this->oldAttributes));
-        return $number_of_changed_attributes > 0 && !$this->owner->isNewRecord;
+        return $number_of_changed_attributes > 0;
     }
-    
+
     public function attributeChanged($attribute_name) {
-    	if(array_key_exists($attribute_name, $this->oldAttributes) && !$this->owner->isNewRecord) {
-    		return $this->oldAttributes[$attribute_name] != $this->owner->$attribute_name;
-    	}
-    	
-    	return false;
+        if(array_key_exists($this->oldAttributes[$attribute_name])) {
+            return $this->oldAttributes[$attribute_name] != $this->owner->$attribute_name;
+        }
+
+        return false;
     }
-    
+
     public function getOldAttributeValue($attribute_name) {
-    	if(array_key_exists($attribute_name, $this->oldAttributes) && !$this->owner->isNewRecord) {
-    		return $this->oldAttributes[$attribute_name];
-    	}
-    	
-    	return null;
+        if(array_key_exists($attribute_name, $this->oldAttributes)) {
+            return $this->oldAttributes[$attribute_name];
+        }
+
+        return null;
     }
     
 }
